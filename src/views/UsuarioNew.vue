ceafb5e10a0b98434aa700a8de0f732c61874a1b
Manuel <53manuelrodriguez@gmail.com>
2023-04-20 11:03:14 -0500

    second commit

--- src/views/UsuarioNew.vue
+++ src/views/UsuarioNew.vue
@@ -6,7 +6,7 @@
                     Registrar posts
                 </div>
                 <div class="card-body">
-                    <form>
+                    <form v-on:submit="guardar">
                         <div class="d-grid col-6 mx-auto mb-3">
                             <img v-if="this.foto" height="100" :src="this.foto" id="fotoimg" class="img-thumbnail" alt="">
                             <img v-else height="100" src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-256.png" class="img-thumbnail" alt="">
@@ -16,6 +16,17 @@
                             <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
                             <input type="text" v-model="nombre" id="nombre" placeholder="Ingrese su post" class="form-control">
                         </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                            <input type="text" v-model="nombre" id="apellido" placeholder="Ingrese su nombre" class="form-control">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-gift"></i></span>
+                            <input v-on:change="previsualizarFoto" type="file" accept="image/png, image/jpeg, image/gif" class="form-control">
+                        </div>
+                        <div class="d-grid col-6 mx-auto mb-3">
+                            <button class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i>Registar</button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -24,31 +35,37 @@
   </template>
 
 <script>
-import axios from 'axios'
-import {confirmar} from '../funciones'
+
+import { assertV8IntrinsicIdentifier } from '@babel/types';
+import {mostrarAlerta,enviarSolicitud} from '../funciones'
 export default {
   data(){
     return{
-      usuarios:null,
+      nombre:'',
+      apellido:'',
+      foto:'',
+      url:'http://127.0.0.1:8000/api/v1/usuarios',
       cargando:false
     }
   },
-  mounted(){
-    this.getUsuarios();
-  },
+  
   methods:{
-    getUsuarios(){
-      this.cargando = true;
-      axios.get('http://127.0.0.1:8000/api/v1/usuarios').then(
-        res => {
-          this.usuarios = res.data;
-          this.cargando = false;
+    guardar(){
+        event.preventDefault();
+        var miFoto = document.getElementById('fotoimg')
+        this.foto = miFoto.src;
+        if(this.nombre.trom()=== ''){
+            mostrarAlerta('Ingrese un nombre','warning','nombre')
         }
-      );
     },
-    eliminar(id,nombre){
-      confirmar('http://127.0.0.1:8000/api/v1/usuarios/',id,'Eliminar Registro','Desea eliminar a '+nombre+'?')
-      this.cargando = false;
+    previsualizarFoto(event){
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0])
+        reader.onload = function(){
+            var miFoto = document.getElementById('fotoimg')
+            miFoto.src = reader.result;
+            
+        }
     }
   }
 }
